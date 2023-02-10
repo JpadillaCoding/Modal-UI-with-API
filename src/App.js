43c042f6827b041c0630009bf4193383b4ef2744
Jose Padilla <jose.apadilla99@gmail.com>
2023-02-09 19:45:18 -0600

    Feat: pulling Data from NASA via JSON

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+// key = TR0olj5zhUcYkIIXzdpmOoIeAeesADDTd9KXQsuY
+
 
 function App() {
+
+const nasaData = "https://images-api.nasa.gov/search?q=hubble&media_type=image"
+const [nasaInfo, setNasaInfo] = useState([])
+
+useEffect(() => {
+  fetch(nasaData)
+  .then(res => res.json())
+  .then(requestedData => setNasaInfo(requestedData))
+  .catch((error) => console.log("Fetch error",error))
+},[])
+
+console.log(nasaInfo.collection)
+
+
   return (
     <div>
       <h1>Working?</h1>
