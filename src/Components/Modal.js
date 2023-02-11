ab3e8cf46b79656674d3c065bad99ec79e6ee7ac
Jose Padilla <jose.apadilla99@gmail.com>
2023-02-11 16:51:04 -0600

    modal now renders and no scroll feature included

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { useState } from "react";
+import "./components.css"
+
+//Future us to make modals components
 
 function Modal(props) {
     const image = props.image
     const info = props.info
 
+    const [modal, setModal] = useState(false)
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+
 
     return (
-        <div className="modal">
-            <div className="overlay"></div>
-            <div className="modal-content">
-                <h1>{info.title}</h1>
-                <h2>{info.description}</h2>
-                <img src={image}/>
+        <div>
+        {modal && (
+            <div className="modal" onClick={toggleModal}>
+                <div className="overlay" onClick={toggleModal}></div>
+                <div className="modal-content">
+                    <h1>{info.title}</h1>
+                    <h2>{info.description}</h2>
+                    <img src={image}/>
+                    <button onClick={toggleModal}>X</button>
+                </div>
             </div>
+            )}
         </div>
     )
 }
--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,15 +1,40 @@
 import React from "react";
-import Modal from "./Modal.js"
+// import Modal from "./Modal.js"
+import { useState } from "react";
 
 function Result(props) {
     const data = props.data
     const image = data.links[0].href
     const info = data.data[0]
-    console.log(info)
+
+    const [modal, setModal] = useState(false)
+    const toggleModal = () => {
+        setModal(!modal)   
+    }
+
+    if (modal) {
+        document.body.classList.add("noScroll")
+    }
+    else {
+        document.body.classList.remove("noScroll")
+    }
+
     return(
         <div className="nasaInfo-item">
-            <img src={image} />
-            <Modal info={info} image={image} />
+            <img src={image} onClick={toggleModal}/>
+            {modal && (
+            <div className="modal">
+                <div className="overlay" onClick={toggleModal}></div>
+                <div className="modal-content">
+                    <h1>{info.title}</h1>
+                    <h2>{info.description}</h2>
+                    <img src={image}/>
+                    <button onClick={toggleModal}>X</button>
+                </div>
+            </div>
+
+            )
+            }
         </div>
     )
 }
