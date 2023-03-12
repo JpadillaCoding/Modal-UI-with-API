94d835e558e8caa7f56d7a62df24d65ea0bf3c91
Jose Padilla <jose.apadilla99@gmail.com>
2023-03-12 08:16:23 -0500

    added comments to parent component- app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import UsePagination from "./Components/UsePagination.js"
 
 function App() {
 
-
+//state is used to manage the search function. To change the default page, the link below can be changed after search?q=.
 const [nasaData, setNasaData] = useState("https://images-api.nasa.gov/search?q=iss&media_type=image")
 const [nasaInfo, setNasaInfo] = useState([])
 
@@ -21,18 +21,20 @@ useEffect(() => {
   setCurrentPage(1)
 }, [nasaData])
 
-
+//Set up of state for pagination. Keeps track of current page and incoming request to change pages.
 const [currentPage, setCurrentPage] = useState(1)
 const [postPerPage, setPostPerPage] = useState(20)
 const lastPostIndex = currentPage * postPerPage
 const firstPostIndex = lastPostIndex - postPerPage
 let currentPost = null;
 
+
+//Conditional added since accessing the "collection" object was throwing errors of properties not being defined. 
 nasaInfo.collection && (currentPost = 
   nasaInfo.collection.items.slice(firstPostIndex, lastPostIndex) 
 )
 
-
+//Components used to render sections of the page. state is passed down as params for each component to manage changes. 
 return (
   <div className='main-wrapper'>
     <Nav setNasaData={setNasaData} />
